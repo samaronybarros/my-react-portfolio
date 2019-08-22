df3a6e3798c1d998ec1e77f9be3253cbd1aaefae
Sam <samarony.barros@gmail.com>
2019-08-22 12:39:39 +0200

    Improves naming

--- src/styles/Navbar.jsx
+++ src/styles/Navbar.jsx
@@ -1,7 +1,7 @@
 import styled, { keyframes } from 'styled-components'
 import S from './constants'
 
-const KFWrapper = keyframes`
+const KFNavBar = keyframes`
     0% {
         opacity: 0;
         transform: translate(550px, 0);
@@ -12,23 +12,23 @@ const KFWrapper = keyframes`
     }
 `
 
-export const DIV = styled.div`
+export const NavBarDiv = styled.div`
     background-color: ${S.colorBackground};
     color: ${S.colorWhite};
     height: 50px;
 
-    animation-name: ${KFWrapper};
+    animation-name: ${KFNavBar};
     animation-duration: 2s;
 `
 
-export const UL = styled.ul`
+export const NavBarUl = styled.ul`
     margin-top: 0;
     list-style: none;
     padding-left: 0;
     text-align: right;
     height: 50px;
 `
-export const LI = styled.ul`
+export const NavBarLi = styled.ul`
     display: inline-block;
     padding: 0 30px;
     line-height: 50px;
@@ -36,7 +36,7 @@ export const LI = styled.ul`
     font-weight: 100;
 `
 
-export const A = styled.a`
+export const NavBarLink = styled.a`
     color: ${S.colorWhite};
     text-decoration: none;
 
