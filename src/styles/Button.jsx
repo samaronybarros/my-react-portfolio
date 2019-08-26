a4a7ac4257065787c2e809d1eddd6be6b3938b78
Sam <samarony.barros@gmail.com>
2019-08-26 17:00:13 +0200

    Adds button style

---
+++ src/styles/Button.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import S from './constants'
+
+export const ButtonStyle = styled.button`
+	background-color: Transparent;
+	background-repeat: no-repeat;
+	color: ${S.colorOrange};
+	border: none;
+	cursor: pointer;
+	overflow: hidden;
+	outline: none;
+	font-size: 18px;
+	font-weight: 400;
+`
