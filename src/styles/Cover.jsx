f75626fc415c91e4e827bdcb0ee5f374ba7186f3
Sam <samarony.barros@gmail.com>
2019-09-24 17:16:50 +0200

    Responsive Cover

--- src/styles/Cover.jsx
+++ src/styles/Cover.jsx
@@ -23,19 +23,23 @@ const KFProfileTitle = keyframes`
     }
 `
 
-export const CoverMain = styled.div`
-    color: ${S.colorWhite};
-`
+export const CoverMain = styled.div`color: ${S.colorWhite};`
 
 export const CoverSpan = styled.span`
-    color: ${S.colorContrast};
-    font-weight: 600;
+	color: ${S.colorContrast};
+	font-weight: 600;
 `
 
 export const CoverDiv = styled.div`
-    display: inline-block;
-    height: 650px;
-    vertical-align: top;
+	display: inline-block;
+	height: 650px;
+	vertical-align: top;
+
+	@media (max-width: 1000px) {
+		display: block;
+		height: auto;
+		vertical-align: top;
+	}
 `
 
 export const ProfilePicture = styled(CoverDiv)`
@@ -44,11 +48,22 @@ export const ProfilePicture = styled(CoverDiv)`
 
     animation-name: ${KFProfilePicture};
     animation-duration: 2s;
+
+    @media (max-width: 1000px) {
+		width: 100%;
+		text-align: center;
+	}
 `
 
 export const ProfilePictureImg = styled.img`
-    border-radius: 50%;
-    margin-top: 100px;
+	border-radius: 50%;
+	margin-top: 100px;
+
+	@media (max-width: 1000px) {
+		border-radius: 50%;
+		margin-top: 50px;
+		max-width: 80%;
+	}
 `
 
 export const ProfileTitle = styled(CoverDiv)`
@@ -56,18 +71,33 @@ export const ProfileTitle = styled(CoverDiv)`
 
     animation-name: ${KFProfileTitle};
     animation-duration: 2s;
+    
+    @media (max-width: 1000px) {
+		width: 100%;
+		text-align: center;
+	}
 `
 
 export const ProfileTitleH1 = styled.h1`
-    font-size: 58px;
-    margin-top: 200px;
-    font-weight: 400;
-    text-transform: uppercase;
+	font-size: 58px;
+	margin-top: 200px;
+	font-weight: 400;
+	text-transform: uppercase;
+
+	@media (max-width: 1000px) {
+		font-size: 50px;
+		font-weight: 400;
+		margin: 50px 20px 0 20px;
+	}
 `
 
 export const ProfileTitleH2 = styled.h2`
-    font-size: 20px;
-    margin-top: -30px;
-    font-weight: 100;
-`
+	font-size: 20px;
+	margin-top: -30px;
+	font-weight: 100;
 
+	@media (max-width: 1000px) {
+		margin: 30px 20px 0 20px;
+		font-size: 25px;
+	}
+`
