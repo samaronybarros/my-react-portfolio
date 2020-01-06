92830faa66b26c242bee12bc1368cd266ae5b775
Sam <samarony.barros@gmail.com>
2020-01-06 21:53:18 +0100

    Adds Typescript

--- src/pages/Curriculum/view/index.jsx
+++ src/pages/Curriculum/view/index.jsx
@@ -8,7 +8,7 @@ import {
 	MainCompanyCollapse,
 	MainParagraph,
 	CVPicture,
-	MainLine,
+	MainLine
 } from '../../../styles/Main'
 import picture from '../../../img/sam-picture.jpg'
 
@@ -24,7 +24,7 @@ import { institutions } from '../../../data/institutions'
 
 class CurriculumView extends Component {
 	state = {
-		showCompanies: false,
+		showCompanies: false
 	}
 
 	handleShowCompanies = () => this.setState({ showCompanies: !this.state.showCompanies })
@@ -62,42 +62,43 @@ class CurriculumView extends Component {
 					<MainLine />
 
 					<MainParagraph>
-						My name is Samarony Barros, but I am also known as Sam Barros. I'm a Senior Software Developer with 8+ years
-						of experience, more specifically working with Java, PHP, JavaScript, C, and C++. I have created systems and
-						customizations to help companies to optimize their process and I have created reports using Business
-						Intelligence (BI) to support companies on making the best decision based on real data, not only empirical
-						data.
+						My name is Samarony Barros, but I am also known as Sam Barros. I'm a Senior Software Developer
+						with 8+ years of experience, more specifically working with Java, PHP, JavaScript, C, and C++. I
+						have created systems and customizations to help companies to optimize their process and I have
+						created reports using Business Intelligence (BI) to support companies on making the best
+						decision based on real data, not only empirical data.
 					</MainParagraph>
 
 					<MainParagraph>
-						Nowadays, I'm responsible for designing, coding, unit testing and debugging application features as
-						specified, applying a keen eye for detail and managing and prioritizing goals to deliver high-quality code.
-						Besides, I also act as a Performance Mentor, coaching Developers to improve their software development
-						skills and how to become more performative in creating queries on databases.
+						Nowadays, I'm responsible for designing, coding, unit testing and debugging application features
+						as specified, applying a keen eye for detail and managing and prioritizing goals to deliver
+						high-quality code. Besides, I also act as a Performance Mentor, coaching Developers to improve
+						their software development skills and how to become more performative in creating queries on
+						databases.
 					</MainParagraph>
 
 					<MainParagraph>
-						In one of my personal projects, I'm working on the development of a tool using MERN Technology (MongoDB,
-						ExpressJS, ReactJS, and NodeJS). The other one is the development of management tools using Spring Boot,
-						Hibernate, REST, Ionic, JWT, S3, MySQL, and MongoDB.
+						In one of my personal projects, I'm working on the development of a tool using MERN Technology
+						(MongoDB, ExpressJS, ReactJS, and NodeJS). The other one is the development of management tools
+						using Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, and MongoDB.
 					</MainParagraph>
 
 					<MainParagraph>
-						In my entrepreneurial career, I had the opportunity to manage projects and different kinds of teams, leading
-						Developers, Consultants, and Designers. As an Entrepreneur, I turned myself into a multidisciplinary and
-						committed professional with the ability to adapt to new situations.
+						In my entrepreneurial career, I had the opportunity to manage projects and different kinds of
+						teams, leading Developers, Consultants, and Designers. As an Entrepreneur, I turned myself into
+						a multidisciplinary and committed professional with the ability to adapt to new situations.
 					</MainParagraph>
 
 					<MainParagraph>
-						I'm proud to have created systems that have contributed to the success and growth of businesses. I'm
-						passionate about personal and professional enhancement. In that way, I'm looking for a challenging
-						opportunity focused on developing and improving the user experience and making a real impact on people's
-						lives.
+						I'm proud to have created systems that have contributed to the success and growth of businesses.
+						I'm passionate about personal and professional enhancement. In that way, I'm looking for a
+						challenging opportunity focused on developing and improving the user experience and making a
+						real impact on people's lives.
 					</MainParagraph>
 
 					<MainParagraph>
-						If you think that I could add value to your network, add me as a contact. You can also find me at
-						samarony.barros@gmail.com and http://sambarros.com
+						If you think that I could add value to your network, add me as a contact. You can also find me
+						at samarony.barros@gmail.com and http://sambarros.com
 					</MainParagraph>
 
 					<MainH2>Work Experience</MainH2>
@@ -159,7 +160,7 @@ class CurriculumView extends Component {
 					<MainH3>4+ years:</MainH3>
 					<MainParagraph>C, C++, Shell Script and Git.</MainParagraph>
 					<MainH3>2+ years:</MainH3>
-					<MainParagraph>Android, Ruby, Objective-C.</MainParagraph>
+					<MainParagraph>Typescript, Android, Ruby, Objective-C.</MainParagraph>
 				</MainDiv>
 				<Footer />
 			</React.Fragment>
